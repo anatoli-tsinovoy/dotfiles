cb8257342b630c61b3fdaed69eea2b353bc37dd1
Anatoli Tsinovoy <anatoli.tsinovoy@gmail.com>
2025-08-19 23:39:33 +0300

    got simply dark working as i'd like

--- aerospace/.config/sketchybar/colors.sh
+++ aerospace/.config/sketchybar/colors.sh
@@ -15,26 +15,69 @@
 # export BG1=0xff363944
 # export BG2=0xff414550
 
-### Catppuccin
-export BLACK=0xff181926
-export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
+## Simply Dark
+export BLACK=0xff110034
+export WHITE=0xfff3f0df
+export RED=0xffff4f44
+export GREEN=0xff00c8ab
+export BLUE=0xff8217ff
+export YELLOW=0xffffd44f
+export ORANGE=0xffffe680
+export MAGENTA=0xffffc9d7
+export GREY=0xff330d81
 export TRANSPARENT=0x00000000
-export BG0=0xff1e1e2e
-export BG1=0x603c3e4f
-export BG2=0x60494d64
+export BG0=0xff110034
+export BG1=0x60330d81
+export BG2=0x609a67ff
 
-export BATTERY_1=0xffa6da95
-export BATTERY_2=0xffeed49f
-export BATTERY_3=0xfff5a97f
-export BATTERY_4=0xffee99a0
-export BATTERY_5=0xffed8796
+export BATTERY_1=0xff00c8ab
+export BATTERY_2=0xffffd44f
+export BATTERY_3=0xffffe680
+export BATTERY_4=0xffff6767
+export BATTERY_5=0xffff4f44
+
+### Simply Light
+## Values from iTerm2 profile `termcolors/simply-brand.itermcolors` (Light) and Cursor `settings.json` Default Light+
+# export BLACK=0xff110034
+# export WHITE=0xfff3f0df
+# export RED=0xffcc3e34
+# export GREEN=0xff009a81
+# export BLUE=0xff330d81
+# export YELLOW=0xffcd9a1b
+# export ORANGE=0xffe6b334
+# export MAGENTA=0xffe69ab3
+# export GREY=0xff330d81
+# export TRANSPARENT=0x00000000
+# export BG0=0xfff3f0df
+# export BG1=0x60b39aff
+# export BG2=0x606734cd
+
+# export BATTERY_1=0xff009a81
+# export BATTERY_2=0xffcd9a1b
+# export BATTERY_3=0xffe6b334
+# export BATTERY_4=0xffcc3e34
+# export BATTERY_5=0xffcc3e34
+
+### Catppuccin
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1e1e2e
+# export BG1=0x603c3e4f
+# export BG2=0x60494d64
+#
+# export BATTERY_1=0xffa6da95
+# export BATTERY_2=0xffeed49f
+# export BATTERY_3=0xfff5a97f
+# export BATTERY_4=0xffee99a0
+# export BATTERY_5=0xffed8796
 
 # General bar colors
 export BAR_COLOR=$BG0
