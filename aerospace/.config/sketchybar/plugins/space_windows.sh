c3835b0ab20c4c7ef3ef1a893739b53a12369d6a
Anatoli Tsinovoy <anatoli.tsinovoy@gmail.com>
2025-09-01 21:44:38 +0300

    everything seems to work again

--- aerospace/.config/sketchybar/plugins/space_windows.sh
+++ aerospace/.config/sketchybar/plugins/space_windows.sh
@@ -3,14 +3,13 @@
 source "$CONFIG_DIR/colors.sh"
 source "$CONFIG_DIR/plugins/map_monitors.sh"
 
-AEROSPACE_FOCUSED_MONITOR=$(aerospace list-monitors --focused | awk '{print $1}')
 AEROSAPCE_WORKSPACE_FOCUSED_MONITOR=$(aerospace list-workspaces --monitor focused --empty no)
 AEROSPACE_EMPTY_WORKSPACE=$(aerospace list-workspaces --monitor focused --empty)
 
 reload_workspace_icon() {
-  local -n args_=$2
-  apps=$(aerospace list-windows --workspace "$@" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-
+  local outvar=$1
+  local args_=()
+  apps=$(aerospace list-windows --workspace "$2" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
   icon_strip=" "
   if [ "${apps}" != "" ]; then
     while read -r app; do
@@ -19,8 +18,31 @@ reload_workspace_icon() {
   else
     icon_strip=" —"
   fi
+  if [ "$4" = 1 ]; then
+    SID_ICON_HIGHLIGHT="true"
+    SID_LABEL_HIGHLIGHT="true"
+    SID_BORDER_COLOR=$GREEN
+    SID_DISPLAY=${SB_AS_MONITOR_MAP["$3"]}
+  else
+    SID_ICON_HIGHLIGHT="false"
+    SID_LABEL_HIGHLIGHT="false"
+    SID_BORDER_COLOR=$BACKGROUND_2
+    if [ -z "$(aerospace list-windows --workspace $2)" ]; then
+      SID_DISPLAY=0
+    else
+      SID_DISPLAY=${SB_AS_MONITOR_MAP["$3"]}
+    fi
+  fi
+  args_+=(--animate sin "10"
+    --set space.$2
+    display="$SID_DISPLAY"
+    label="$icon_strip"
+    icon.highlight="$SID_ICON_HIGHLIGHT"
+    label.highlight="$SID_LABEL_HIGHLIGHT"
+    background.border_color="$SID_BORDER_COLOR"
+  )
 
-  args_+=(--animate sin 10 --set space.$@ label="$icon_strip" display=${SB_AS_MONITOR_MAP["$3"]})
+  eval "$outvar+=(\"\${args_[@]}\")"
 }
 
 if [ "$SENDER" = "aerospace_workspace_change" ]; then
@@ -31,50 +53,16 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
     fi
   done
 
-  # The simplest solution here is just to basically rebuild the entire 'spaces' item, on each monitor, as we do in spaces.sh
-  # if [ $i = "$FOCUSED_WORKSPACE" ]; then
-  #   sketchybar --set space.$FOCUSED_WORKSPACE background.drawing=on
-  # else
-  #   sketchybar --set space.$FOCUSED_WORKSPACE background.drawing=off
-  # fi
-  #echo 'space_windows_change: '$AEROSPACE_FOCUSED_WORKSPACE >> ~/aaaa
-  #echo space: $space >> ~/aaaa
-  #space="$(echo "$INFO" | jq -r '.space')"
-  #apps="$(echo "$INFO" | jq -r '.apps | keys[]')"
-  # apps=$(aerospace list-windows --workspace $AEROSPACE_FOCUSED_WORKSPACE | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-  #
-  # icon_strip=" "
-  # if [ "${apps}" != "" ]; then
-  #   while read -r app
-  #   do
-  #     icon_strip+=" $($CONFIG_DIR/plugins/icon_map.sh "$app")"
-  #   done <<< "${apps}"
-  # else
-  #   icon_strip=" —"
-  # fi
-
-  reload_workspace_icon "$AEROSPACE_PREV_WORKSPACE" args $AEROSPACE_PREV_MONITOR
-  reload_workspace_icon "$AEROSPACE_FOCUSED_WORKSPACE" args $AEROSPACE_FOCUSED_MONITOR
-
-  # current workspace space border color
-  args+=(--set space.$AEROSPACE_FOCUSED_WORKSPACE
-    icon.highlight=true
-    label.highlight=true
-    background.border_color=$GREEN
-  )
-  # prev workspace space border color
-  args+=(--set space.$AEROSPACE_PREV_WORKSPACE
-    icon.highlight=false
-    label.highlight=false
-    background.border_color=$BACKGROUND_2
-  )
-
   for i in $AEROSPACE_EMPTY_WORKSPACE; do
     if [ "$i" -eq $AEROSPACE_FOCUSED_WORKSPACE ]; then
       continue
     fi
     args+=(--set space.$i display=0)
   done
+
+  AEROSPACE_FOCUSED_MONITOR=$(aerospace list-monitors --focused | awk '{print $1}')
+  reload_workspace_icon args $AEROSPACE_PREV_WORKSPACE $AEROSPACE_PREV_MONITOR 0
+  reload_workspace_icon args $AEROSPACE_FOCUSED_WORKSPACE $AEROSPACE_FOCUSED_MONITOR 1
 
   if [ ${#args[@]} -gt 0 ]; then
     sketchybar "${args[@]}"
