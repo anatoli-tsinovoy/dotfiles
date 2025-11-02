56a23efa856d4e02db73da88fe2bdd888b3b6e97
Anatoli Tsinovoy <anatoli.tsinovoy@gmail.com>
2025-11-02 15:47:25 +0200

    comment out unused aerospace queries to reduce latency

--- aerospace/.config/sketchybar/plugins/front_app.sh
+++ aerospace/.config/sketchybar/plugins/front_app.sh
@@ -5,8 +5,8 @@
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-AEROSPACE_FOCUSED_MONITOR_NO=$(aerospace list-workspaces --focused)
-AEROSPACE_LIST_OF_WINDOWS_IN_FOCUSED_MONITOR=$(aerospace list-windows --workspace $AEROSPACE_FOCUSED_MONITOR_NO | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+# AEROSPACE_FOCUSED_MONITOR_NO=$(aerospace list-workspaces --focused)
+# AEROSPACE_LIST_OF_WINDOWS_IN_FOCUSED_MONITOR=$(aerospace list-windows --workspace $AEROSPACE_FOCUSED_MONITOR_NO | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
 if [ "$SENDER" = "front_app_switched" ]; then
   # apps=$AEROSPACE_LIST_OF_WINDOWS_IN_FOCUSED_MONITOR
