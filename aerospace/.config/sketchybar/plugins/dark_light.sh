e9d115c0f53eef5af8038623264449ab92e567c3
Anatoli Tsinovoy <anatoli.tsinovoy@gmail.com>
2026-08-23 15:22:23 +0300

    Revert SketchyBar restart changes

--- aerospace/.config/sketchybar/plugins/dark_light.sh
+++ aerospace/.config/sketchybar/plugins/dark_light.sh
@@ -14,5 +14,7 @@ current_theme="$(detect_theme)"
 previous_theme="$(sketchybar --query DARK_LIGHT | jq -r '.label.value')"
 sketchybar --set DARK_LIGHT label="$current_theme" display=0
 if [ "$SENDER" != "forced" ] && [ "$previous_theme" != "$current_theme" ]; then
-  aerospace trigger-binding alt-shift-b --mode service
+  # sketchybar --reload # for some odd reason this increments the number of bars in play on every theme change
+  # Note that this has a 10-second back-off time set by the crash-loop detection in sketchybar's plist, so fast-toggling is off the table
+  launchctl kickstart -kp "gui/$UID/homebrew.mxcl.sketchybar"
 fi
